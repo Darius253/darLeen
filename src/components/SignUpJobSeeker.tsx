2fe07d8ba831c9b46f479e3a32aea4a36db427c6
Darius Tron <dariustron@Dariuss-MacBook-Pro.local>
2024-05-07 01:47:17 +0100

    Added Sign Up pages and HomePages

---
+++ src/components/SignUpJobSeeker.tsx
@@ -0,0 +1,270 @@
+import { Link } from "react-router-dom";
+import ImageSrc from "../assets/login.jpg";
+
+export const SignUpJobSeeker = () => {
+  return (
+    <div
+      className="row"
+      style={{
+        minHeight: "100vh",
+      }}
+    >
+      <div className="row">
+        <div
+          className="col col-6"
+          style={{
+            backgroundImage: `url(${ImageSrc})`,
+            backgroundSize: "cover",
+          }}
+        ></div>
+        <div
+          className="col  d-flex align-items-center justify-content-center"
+          style={{ backgroundColor: "white" }}
+        >
+          <form>
+            <div
+              className="d-flex    align-items-center justify-content-start"
+              style={{ marginBottom: "10px" }}
+            >
+              <a style={{ fontSize: "17px", fontWeight: "bold" }}>
+                Create an Account with Us.
+              </a>
+              <Link to="../signup">
+                <button
+                  className="btn btn-link"
+                  style={{
+                    fontSize: "15px",
+                    fontWeight: "bold",
+                    color: "#0086CA",
+                    marginLeft: "100px",
+                  }}
+                >
+                  Sign Up as a Company here!
+                </button>
+              </Link>
+            </div>
+            <div className="row" style={{ marginBottom: "30px" }}>
+              <div className="col">
+                <input
+                  type="email"
+                  className="form-control"
+                  placeholder="Email"
+                  aria-label="Email"
+                  id="email"
+                  required
+                />
+              </div>
+              <div className="col">
+                <input
+                  type="password"
+                  className="form-control"
+                  placeholder="Password"
+                  aria-label="Password"
+                  id="password"
+                  required
+                />
+              </div>
+            </div>
+
+            <div className="row" style={{ marginBottom: "30px" }}>
+              <div className="col">
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="First name"
+                  aria-label="First name"
+                  id="firstName"
+                  required
+                />
+              </div>
+              <div className="col">
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Last name"
+                  aria-label="Last name"
+                  id="lastName"
+                  required
+                />
+              </div>
+            </div>
+            <div className="row" style={{ marginBottom: "30px" }}>
+              <div className="col">
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Education qualification"
+                  aria-label="Education qualification"
+                  required
+                />
+              </div>
+
+              <div className="col">
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Years of experience"
+                  aria-label="Years of experience"
+                  required
+                  value=""
+                />
+              </div>
+            </div>
+            <div className="row" style={{ marginBottom: "30px" }}>
+              <div className="col">
+                <input
+                  type="location"
+                  className="form-control"
+                  placeholder="Location"
+                  aria-label="Location"
+                  id="location"
+                  required
+                />
+              </div>
+              <div className="col">
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Minimum education level"
+                  aria-label="Minimum education level"
+                  required
+                />
+              </div>
+              <div className="col">
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Job preference"
+                  aria-label="Job preference"
+                  required
+                />
+              </div>
+            </div>
+
+            <div className="row" style={{ marginBottom: "30px" }}>
+              <div className="col">
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Skill"
+                  aria-label="Skill"
+                  required
+                />
+              </div>
+              <div className="col">
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Skill"
+                  aria-label="Skill"
+                  required
+                />
+              </div>
+              <div className="col">
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Skill"
+                  aria-label="Skill"
+                  required
+                />
+              </div>
+              <div className="col">
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Skill"
+                  aria-label="Skill"
+                  required
+                />
+              </div>
+            </div>
+            <div className="row" style={{ marginBottom: "30px" }}>
+              <div className="col">
+                <textarea
+                  className="form-control"
+                  placeholder="Professional summary"
+                  aria-label="Professional summary"
+                  required
+                  maxLength={630}
+                />
+              </div>
+            </div>
+
+            <div
+              style={{
+                width: "360px",
+                marginBottom: "30px",
+              }}
+            >
+              <div className="d-flex   flex-row align-items-center justify-content-start">
+                <input type="radio" style={{ marginRight: "8px" }} />
+                <a
+                  style={{
+                    color: "#9B9B9B",
+                    fontSize: "14px",
+                  }}
+                >
+                  I have read and agree to DarLeen's
+                </a>
+              </div>
+              <button
+                className="btn btn-link"
+                style={{
+                  fontSize: "14px",
+                  color: "#0086CA",
+                  marginLeft: "8px",
+                }}
+              >
+                Privacy Policy, Terms of Use, and Cookies Policy
+              </button>
+            </div>
+
+            <Link to="/homepage/jobseeker">
+              {" "}
+              <button
+                type="submit"
+                className="btn btn"
+                style={{
+                  color: "white",
+                  fontSize: "16px",
+                  fontWeight: "bold",
+                  textAlign: "center",
+                  backgroundColor: "#0086CA",
+                  width: "360px",
+                  marginLeft: "30%",
+                }}
+              >
+                Sign Up
+              </button>
+            </Link>
+
+            <div style={{ marginLeft: "40%", marginTop: "50px" }}>
+              <a style={{ fontSize: "15px", color: "#4A4A4A" }}>
+                Already have an account?
+              </a>
+
+              <Link to="../login">
+                <button
+                  className="btn btn-link"
+                  style={{
+                    fontSize: "15px",
+                    fontWeight: "bold",
+                    color: "#0086CA",
+                  }}
+                >
+                  Sign In
+                </button>
+              </Link>
+            </div>
+            <div style={{ marginLeft: "50%", marginTop: "50px" }}>
+              <a style={{ fontSize: "12px", color: "#4A4A4A" }}>
+                © 2024 DarLeen, Inc.
+              </a>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
